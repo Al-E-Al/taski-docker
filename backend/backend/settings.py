6ca30e6c91352c6fb81be2926e5c1493224319d4
Алексей Ершов <al.ershov84@gmail.com>
2023-11-11 01:32:11 +0300

    Add fin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['158.160.73.175', '127.0.0.1', 'localhost', 'iskiiskt.myddns.me']
+ALLOWED_HOSTS = ['158.160.73.175', '127.0.0.1', 'localhost', 'https://kitikit.myddns.me.ru']
 
 
 # Application definition
