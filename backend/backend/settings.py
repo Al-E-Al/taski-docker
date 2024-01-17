44de5d3b4abf0cb582cb8d47393f52da78bbd4c4
Алексей Ершов <al.ershov84@gmail.com>
2024-01-17 04:34:44 +0300

    taski1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['158.160.73.175', '127.0.0.1', 'localhost', 'https://kitikit.myddns.me.ru']
-
+# ALLOWED_HOSTS = ['158.160.73.175', '127.0.0.1', 'localhost', 'https://kitikit.myddns.me']
+ALLOWED_HOSTS = ['158.160.73.175', '127.0.0.1', 'localhost', 'https://iskiiskt.myddns.me']
 
 # Application definition
 
